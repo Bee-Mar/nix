9a4db944cd714d9220ce7e7655bba580bf1f8c2f
Brandon Marlowe <brandon.marlowe@radiancetech.com>
2024-02-01 11:12:51 -0500

    inverted imports

--- modules.nix
+++
@@ -1,7 +0,0 @@
-{ lib, ... }:
-{
-  imports =
-    [ ./base/configuration.nix ]
-    ++ lib.optional (builtins.pathExists ./custom/configuration.nix) ./custom/configuration.nix
-    ++ lib.optional (builtins.pathExists ./work/configuration.nix) ./work/configuration.nix;
-}
