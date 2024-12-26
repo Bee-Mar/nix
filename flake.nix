309b6f72723ff04dd7db23c4891c162e882592af
Brandon Marlowe <bpmarlowe-software@protonmail.com>
2024-12-26 18:08:32 -0500

    update to nix 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake-Based Nix/NixOS Configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
   };
 
   outputs = { self, nixpkgs }:
